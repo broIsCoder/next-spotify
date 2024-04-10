fdbd704b249b8924262412e002921d560df4be76
dhungelsafal <dhungelsafal@gmail.com>
2024-04-10 10:29:23 +0000

    changed redirect url

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     env: {
         NEXTAUTH_SECRET:'eTFwvfYBE3/ZAGdGZSYShNdk9Q6FYBIS4HKBk+vQYp4=',
-        NEXTAUTH_URL:'http://localhost:3000/api/auth',
+        NEXTAUTH_URL:'https://9000-monospace-next-spotify-1712744097515.cluster-22qpi2wzsjc4utjzyqn2yu6ar6.cloudworkstations.dev/?monospaceUid=502723&embedded=0/api/auth',
         SPOTIFY_SECRET:'ab8d4686e0844e8490ec44204a521cdb',
         SPOTIFY_ID:'319cb91ecf0f4314ba3212924959072e',
         JWT_SECRET:'mysign',
-        REDIRECT_URI:'http://localhost:3000/api/auth/callback/spotify',
+        REDIRECT_URI:'https://9000-monospace-next-spotify-1712744097515.cluster-22qpi2wzsjc4utjzyqn2yu6ar6.cloudworkstations.dev/?monospaceUid=502723&embedded=0/api/auth/callback/spotify',
     },
   }
